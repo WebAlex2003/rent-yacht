a47ec4a0f5e9fc56ea6dc743e2733b41d125cd8d
WebAlex2003 <nikolenkolesha30032003@gmail.com>
2025-12-17 14:22:55 +0100

    json-path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: '/rent-yacht/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
